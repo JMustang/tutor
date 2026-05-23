ee621cd0e5ea6ba373c585df2748854c02377404
Junior <eecfredes@gmail.com>
2026-05-23 19:34:15 -0300

    EXEC 02: Functions

--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -1,9 +1,19 @@
 // TODO: Fix the function body without changing the signature.
 fn square(num: i32) -> i32 {
-    num * num;
+    num * num
 }
 
 fn main() {
     let answer = square(3);
     println!("The square of 3 is {answer}");
 }
+
+/*
+    Em Rust, a última expressão de uma função (sem ponto e vírgula)
+    é retornada implicitamente. Quando você coloca ; após num * num,
+    você transforma aquela expressão em uma declaração (statement),
+    que retorna () (a tupla vazia, também chamada de "unit").
+
+    Como a função declarou retorno -> i32, mas o corpo efetivamente retorna (),
+    o compilador aponta um erro de tipo:
+*/
